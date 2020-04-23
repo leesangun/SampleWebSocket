8e18d05d2c68f2aafea55c6f4b2b41d68fe685c1
leesangun <lsuone@naver.com>
2020-04-23 19:14:22 +0900

    first

---
+++ Client/ClientCShop0/ClientCShop0/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WatsonWebsocket;
+
+//NuGet WatsonWebsocket
+
+namespace ClientCShop0
+{
+    class ServerWatsonWs
+    {
+        WatsonWsClient _client = new WatsonWsClient("127.0.0.1", 8181, false);
+
+        public ServerWatsonWs()
+        {
+            _client.ServerConnected += ServerConnected;
+            _client.ServerDisconnected += ServerDisconnected;
+            _client.MessageReceived += MessageReceived;
+            _client.Start();
+        }
+
+         void MessageReceived(object sender, MessageReceivedEventArgs args)
+        {
+            Console.WriteLine("Message from server: " + Encoding.UTF8.GetString(args.Data));
+        }
+
+         void ServerConnected(object sender, EventArgs args)
+        {
+            Console.WriteLine("Server connected");
+        }
+
+         void ServerDisconnected(object sender, EventArgs args)
+        {
+            Console.WriteLine("Server disconnected");
+        }
+
+        public void Send()
+        {
+            _client.SendAsync(Encoding.UTF8.GetBytes("test"));
+        }
+    }
+
+
+    static class Program
+    {
+        public static ServerWatsonWs _ws;
+
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            _ws = new ServerWatsonWs();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Server/ServerCShop0/ServerCShop0/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WatsonWebsocket;
+
+//NuGet WatsonWebsocket
+
+namespace ServerCShop0
+{
+    class Client{
+        private readonly string _ipPort;
+        WatsonWsServer _server;
+        public Client(string ipPort, WatsonWsServer server)
+        {
+            _ipPort = ipPort;
+            _server = server;
+        }
+
+        public void OnConnect()
+        {
+            Console.WriteLine("Client connected: " + _ipPort);
+        }
+
+        public void OnDisconnect()
+        {
+            Console.WriteLine("Client disconnected: " + _ipPort);
+        }
+
+        public void OnReceive(byte[] bytes)
+        {
+            Console.WriteLine("Message received from " + _ipPort + ": " + Encoding.UTF8.GetString(bytes));
+        }
+
+        public void Send(byte[] bytes)
+        {
+            _server.SendAsync(_ipPort, bytes);
+        }
+
+    }
+
+    class ServerWatsonWs
+    {
+        WatsonWsServer _server = new WatsonWsServer("127.0.0.1", 8181, false);
+        private ConcurrentDictionary<string, Client> _clients = new ConcurrentDictionary<string, Client>();
+        public ServerWatsonWs()
+        {
+            _server.ClientConnected += ClientConnected;
+            _server.ClientDisconnected += ClientDisconnected;
+            _server.MessageReceived += MessageReceived;
+            _server.Start();
+        }
+
+        private void ClientConnected(object sender, ClientConnectedEventArgs args)
+        {
+            _clients.TryRemove(args.IpPort, out var removed);
+            Client client = new Client(args.IpPort,_server);
+            _clients.TryAdd(args.IpPort, client);
+            client.OnConnect();
+
+            
+        }
+
+        private void ClientDisconnected(object sender, ClientDisconnectedEventArgs args)
+        {
+            if (_clients.TryGetValue(args.IpPort, out Client client))
+            {
+                client.OnDisconnect();
+                _clients.TryRemove(args.IpPort, out var removed);
+            } 
+        }
+
+        private void MessageReceived(object sender, MessageReceivedEventArgs args)
+        {
+            if (_clients.TryGetValue(args.IpPort, out Client client))
+            {
+                client.OnReceive(args.Data);
+
+                // client.Send(Encoding.UTF8.GetBytes("Server to Client"));
+                client.Send(args.Data);
+            }
+
+            
+        }
+    }
+
+    static class Program
+    {
+        
+
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ServerWatsonWs ws = new ServerWatsonWs();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+
+        
+    }
+}
